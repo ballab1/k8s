92b8f2da39845d4983b2d1bdc3473c9940196045
Ballantyne, Bob <ballantyne.robert@gmail.com>
2022-01-18 13:05:59 -0500

    improvements to reinit

--- get_apis.sh
+++ get_apis.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
-set -o errexit
-set -o nounset
-set -o pipefail
-IFS=\$'\n\t'
-source /home/bobb/.bin/trap.bashlib
-trap.__init 
+function main() {
 
-declare mode object
-declare -a modes=('wide' 'json' 'yaml')
+    set -o errexit
+    set -o nounset
+    set -o pipefail
+    IFS=\$'\n\t'
+    source /home/bobb/.bin/trap.bashlib
+    trap.__init 
 
-mkdir -p apis
-rm -rf apis/*
-for mode in "${modes[@]}"; do
-    mkdir -p "apis/$mode"
-done
 
-while read -r object; do
-    echo "  $object"
+    local mode object
+    local -a modes=('wide' 'json' 'yaml')
+
+    mkdir -p apis
+    rm -rf apis/*
     for mode in "${modes[@]}"; do
-        declare ext="$mode"
-        [ "$mode" = 'wide' ] && ext='txt'
-        microk8s kubectl get "$object" -A -o "$mode" &> "apis/${mode}/${object}.$ext" ||:
+        mkdir -p "apis/$mode"
     done
-done < <(microk8s.kubectl api-resources --no-headers=false -o=name --sort-by=name)
+
+    while read -r object; do
+        echo "  $object"
+        for mode in "${modes[@]}"; do
+            declare ext="$mode"
+            [ "$mode" = 'wide' ] && ext='txt'
+            microk8s kubectl get "$object" -A -o "$mode" &> "apis/${mode}/${object}.$ext" ||:
+        done
+    done < <(microk8s.kubectl api-resources --no-headers=false -o=name --sort-by=name)
+}
+
+main "$@"
--- reinit.sh
+++ reinit.sh
@@ -3,11 +3,170 @@
 VERSION=1.23
 
 # -----------------------------------------------------------------------------------
+function add_nodes_to_cluster() {
+
+    echo 'join nodes into cluster'
+    local cmd
+    for h in {6..8}; do
+        cmd="$(sudo microk8s add-node | grep '10.3.1.14' | head -1)"
+        echo "ssh s$h '$cmd'"
+        run ssh "s$h" "sudo $cmd"
+        run ssh "s$h" "microk8s status --wait-ready"
+    done
+    logElapsed "${FUNCNAME[0]}"
+}
+
+# -----------------------------------------------------------------------------------
+function customize_content() {
+
+    echo 'customizing content'
+    run microk8s.kubectl apply -f ./production/corends.ConfigMap.yml
+    run microk8s.kubectl apply -f ./production/nginx-ingress.DaemonSet.yml
+    run microk8s.kubectl apply -f ./production/grafana.Deployment.yml
+    run microk8s.kubectl apply -f ./production/grafana.Service.yml
+    run microk8s.kubectl apply -f ./production/kubernetes-dashboard.Deployment.yml
+    run microk8s.kubectl apply -f ./production/kubernetes-dashboard.Service.yml
+    run microk8s.kubectl apply -f ./production/kubernetes-dashboard-ingress.Ingress.yml
+
+    run microk8s.kubectl create -f ./production/local-hd.StorageClass.yml
+    run microk8s.kubectl create -f ./production/s5-local.PersistentVolume.yml
+    run microk8s.kubectl create -f ./production/s6-local.PersistentVolume.yml
+    run microk8s.kubectl create -f ./production/s7-local.PersistentVolume.yml
+    run microk8s.kubectl create -f ./production/s8-local.PersistentVolume.yml
+#    run microk8s.kubectl create -f ./production/my-claim.persistentVolumeClaim.yml
+
+    run microk8s.kubectl create -f ./production/jenkins.Namespace.yml
+    run microk8s.kubectl create -f ./production/jenkins-root-ca.Secret.yml
+    run microk8s.kubectl create -f ./production/jenkins-server-ca.Secret.yml
+    logElapsed "${FUNCNAME[0]}"
+}
+
+# -----------------------------------------------------------------------------------
+function capture_config() {
+
+    echo 'capturing config content'
+    microk8s config > kube.config
+    cp kube.config /home/bobb/.kube/config
+    microk8s.kubectl api-versions ||: > ./api-api-versions.txt
+    microk8s.kubectl api-resources -o wide ||: > ./api-resources.txt
+    logElapsed "${FUNCNAME[0]}"
+}
+
+# -----------------------------------------------------------------------------------
+function capture_ips_for_gui() {
+
+    echo 'capturing IPs for s3.ubuntu.home'
+    microk8s.kubectl get services -A | awk '{if (substr($4,0,4) == '10.1') {sub(/\/[TCP|UDP].+$/,"",$6);sub(/:.*$/,"",$6); print "{|title|:|" $2 "|,|host|:|" $4 "|,|port|:|" $6 "|}" }}'|sed -e 's/|/"/g'|jq -s > services_ips.json
+    scp services_ips.json s3:production/workspace.production/www/
+    logElapsed "${FUNCNAME[0]}"
+}
+
+# -----------------------------------------------------------------------------------
+function capture_status() {
+
+    echo 'capturing status'
+    sudo snap alias microk8s.kubectl kubectl 
+    run microk8s.kubectl cluster-info
+    run microk8s.kubectl get nodes
+    run microk8s.kubectl get all -A
+
+#    ./get_apis.sh &
+
+    logElapsed "${FUNCNAME[0]}"
+}
+
+# -----------------------------------------------------------------------------------
+function enable_micrk8s_modules() {
+
+    echo 'enable microk8s modules'
+    for module in dashboard dashboard-ingress dns fluentd ingress 'metallb:10.64.140.43-10.64.140.49' metrics-server prometheus storage; do
+        run sudo microk8s enable "$module" ||:
+    done
+    run microk8s status --wait-ready
+    run sudo iptables -P FORWARD ACCEPT
+    logElapsed "${FUNCNAME[0]}"
+}
+
+# -----------------------------------------------------------------------------------
+function generate_token() {
+
+    echo 'generating tokens.inf'
+    declare token_file='tokens.inf'
+    :> "$token_file"
+    for token in default-token admin-user ; do
+        declare ref="$(microk8s.kubectl -n kube-system get secret | grep "$token" | awk '{print $1}')"
+        [ -z "${ref:-}" ] && continue
+        run microk8s.kubectl -n kube-system describe secret "$ref" | tee -a "$token_file"
+        break
+    done
+    logElapsed "${FUNCNAME[0]}"
+}
+
+# -----------------------------------------------------------------------------------
+function logElapsed() {
+
+    [ "${DEBUG:-0}" -ne 0 ] || return
+    local -r text="${1:-}"
+    local current=$(timer.getTimestamp)
+
+    {
+        timer.logElapsed "$text" $((current - BEGIN)) 
+        echo
+        BEGIN="$current"
+    } | tee -a elapsed.times.txt
+}
+
+# -----------------------------------------------------------------------------------
+function main() {
+
+    START=$(date +%s)
+    BEGIN="$START"
+    DEBUG=1
+    :> elapsed.times.txt
+
+    # Use the Unofficial Bash Strict Mode
+    set -o errexit
+    set -o nounset
+    set -o pipefail
+    IFS=$'\n\t'
+    source /home/bobb/.bin/trap.bashlib
+    source /home/bobb/.bin/utilities/bashlib/timer.bashlib
+    trap.__init
+    trap onexit EXIT
+
+    sudo usermod -a -G microk8s "$USER"
+    mkdir -p /home/bobb/.kube
+    sudo chown -f -R "$USER" /home/bobb/.kube
+
+    reinit_nodes
+    add_nodes_to_cluster
+    enable_micrk8s_modules
+    customize_content
+    generate_token
+    capture_config
+    capture_ips_for_gui
+    capture_status
+}
+
+# -----------------------------------------------------------------------------------
+function onexit() {
+
+    find /tmp -maxdepth 1 -mindepth 1 -name 'ssh.s?.txt' -type f -delete
+    for h in {5..8}; do
+        ssh "s$h" "[ -e '$SCRIPT' ] && rm '$SCRIPT'"
+    done
+
+    local current=$(timer.getTimestamp)
+    {
+        timer.logElapsed 'Total Elapse time: ' $((current - START)) 
+        echo
+    } | tee -a elapsed.times.txt
+}
+
+# -----------------------------------------------------------------------------------
 function onRemote() {
     cat << "EOF"
 #!/bin/bash
-echo "## $(hostname -f) ####################################################################"
-
 function add_registries() {
 
     cat << "REGISTRY"
@@ -17,7 +176,7 @@ function add_registries() {
         endpoint = ["http://10.3.1.12:5000"] 
 REGISTRY
 }
-# -----------------------------------------------------------------------------------
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 function create_registry_defs() {
 
     local SNAP_DATA="${1:-/etc/containerd/config.toml}"
@@ -35,7 +194,57 @@ server = "$reg"
 REGISTRY
     done
 }
-# -----------------------------------------------------------------------------------
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+function main() {
+
+    START=$(date +%s)
+    DEBUG=1
+
+    local host="${1:?}"
+    local version="${2:?}"
+    echo "## ${host} ###########################################################################"
+
+    # Use the Unofficial Bash Strict Mode
+    set -o errexit
+    set -o nounset
+    set -o pipefail
+    IFS=$'\n\t'
+    source /home/bobb/.bin/trap.bashlib
+    source /home/bobb/.bin/utilities/bashlib/timer.bashlib
+    trap.__init
+    trap onexit EXIT
+
+    run snap remove microk8s
+    run snap install microk8s --channel "${version}/stable" --classic
+
+    run microk8s stop
+    local SNAP='/var/snap/microk8s/current'
+    echo 'add k8s.home to csr.conf.template'
+    sed -i -e '/cluster.local/aDNS.6 = k8s.home' "${SNAP}/certs/csr.conf.template"
+
+    if [ "$version" = '1.23' ]; then
+        echo "create_registry_defs for ${version}"
+        run create_registry_defs "$SNAP" "${version}"
+    else
+        echo "add_registries for ${version}"
+        run add_registries "${version}" >> "${SNAP}/args/containerd-template.toml"
+    fi
+    run microk8s start
+    run microk8s refresh-certs
+    echo 'waiting for K8s to be ready'
+    run microk8s status --wait-ready
+    echo "## ${host} ##################################################################### END #"
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+function onexit() {
+
+    [ "${DEBUG:-0}" -ne 0 ] || return
+
+    local current=$(timer.getTimestamp)
+    local -i elapsed=$((current - START)) 
+        printf 'Elapsed time: %s\n' "$(timer.fmtElapsed $elapsed)" 
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 function run() {
   {
       echo
@@ -44,127 +253,51 @@ function run() {
   } >&2
   "$@"
 }
-
-# -----------------------------------------------------------------------------------
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 EOF
     # start new HEREDOC with substitution
     cat << EOF
-
-# Use the Unofficial Bash Strict Mode
-set -o errexit
-set -o nounset
-set -o pipefail
-IFS=\$'\n\t'
-source /home/bobb/.bin/trap.bashlib
-trap.__init
-
-run snap remove microk8s
-run snap install microk8s --channel "${VERSION}/stable" --classic
-
-run microk8s stop
-declare SNAP='/var/snap/microk8s/current'
-echo 'add k8s.home to csr.conf.template'
-sed -i -e '/cluster.local/aDNS.6 = k8s.home' "\${SNAP}/certs/csr.conf.template"
-
-if [ "$VERSION" = '1.23' ]; then
-    echo "create_registry_defs for ${VERSION}"
-    run create_registry_defs "\$SNAP" "${VERSION}"
-else
-    echo "add_registries for ${VERSION}"
-    run add_registries "${VERSION}" >> "\${SNAP}/args/containerd-template.toml"
-fi
-run microk8s start
-echo 'waiting for K8s to be ready'
-run microk8s status --wait-ready
-echo "## $(hostname -f) ############################################################## END #"
+SCRIPT="\$0"
+main "\$(hostname)" "$VERSION"
 EOF
 }
 
 # -----------------------------------------------------------------------------------
-function main() {
+function reinit_nodes() {
 
-    # Use the Unofficial Bash Strict Mode
-    set -o errexit
-    set -o nounset
-    set -o pipefail
-    IFS=$'\n\t'
-    source /home/bobb/.bin/trap.bashlib
-    trap.__init
-
-    sudo usermod -a -G microk8s "$USER"
-    mkdir -p /home/bobb/.kube
-    sudo chown -f -R "$USER" /home/bobb/.kube
+    SCRIPT="$(mktemp)"
+    onRemote > "$SCRIPT"
 
     local -r script='/tmp/reinit'
-    local -r tmp_script="$(mktemp)"
-    onRemote > "$tmp_script"
-    echo '-- remote_script: /tmp/reinit --------------------------------------------------------'
-    cat "$tmp_script"
-    echo '-- remote_script: /tmp/reinit -------------------------------------------------- EOF -'
+#    echo "-- remote_script: $script --------------------------------------------------------"
+#    cat "$SCRIPT"
+#    echo "-- remote_script: $script -------------------------------------------------- EOF -"
 
+    echo 'reinit each node'
     for h in {5..8}; do
-        run scp "$tmp_script" "s$h:$script"
+      (
+        run scp "$SCRIPT" "s$h:$script"
         ssh "s$h" "chmod 755 $script"
         run ssh "s$h" "sudo $script"
-        ssh "s$h" "rm $script"
-    done
-    rm "$tmp_script"
-
-    local cmd
-    for h in 6 7 8; do
-        cmd="$(sudo microk8s add-node | grep '10.3.1.14' | head -1)"
-        echo "ssh s$h '$cmd'"
-        run ssh "s$h" "sudo $cmd"
-        run ssh "s$h" "microk8s status --wait-ready"
+        ssh "s$h" "[ -e "$script" ] && rm $script"
+      ) &> /tmp/ssh.s$h.txt &
     done
 
-    for module in dashboard dashboard-ingress dns fluentd ingress 'metallb:10.64.140.43-10.64.140.49' metrics-server prometheus storage; do
-        run sudo microk8s enable "$module" ||:
-    done
-    run microk8s status --wait-ready
-    run sudo iptables -P FORWARD ACCEPT
-
-    echo 'creating custom content'
-    run microk8s.kubectl apply -f ./production/nginx-ingress.yml
-    run microk8s.kubectl apply -f ./production/corends.yml
-    run microk8s.kubectl apply -f ./production/grafana.yml
-    run microk8s.kubectl apply -f ./production/dashboard.yml
-
-    run microk8s.kubectl create -f ./production/local-hd-storage.yml
-    run microk8s.kubectl create -f ./production/s5-local-hd.yml
-    run microk8s.kubectl create -f ./production/s6-local-hd.yml
-    run microk8s.kubectl create -f ./production/s7-local-hd.yml
-    run microk8s.kubectl create -f ./production/s8-local-hd.yml
-#    run microk8s.kubectl create -f ./production/persistentVolumeClaim.yml
-
-    run microk8s.kubectl create -f ./production/jenkins-ns.yml
-    run microk8s.kubectl create -f ./production/jenkins-root-ca.yml
-    run microk8s.kubectl create -f ./production/jenkins-server-ca.yml
-
-    microk8s config > kube.config
-    cp kube.config /home/bobb/.kube/config
-    microk8s.kubectl api-resources -o wide > ./api-resources.txt
-    microk8s api-versions > ./api-api-versions.txt
-    echo
-
-    declare token_file='tokens.inf'
-    :> "$token_file"
-    for token in default-token admin-user ; do
-        declare ref="$(microk8s.kubectl -n kube-system get secret | grep "$token" | awk '{print $1}')"
-        [ -z "${ref:-}" ] && continue
-        run microk8s.kubectl -n kube-system describe secret "$ref" | tee -a "$token_file"
-        break
+    echo '  wait for all reinit commands to complete'
+    while true; do
+        wait
+        [ "$(jobs)" ] || break
     done
 
-    microk8s.kubectl get services -A | awk '{if (substr($4,0,4) == '10.1') {sub(/\/[TCP|UDP].+$/,"",$6);sub(/:.*$/,"",$6); print "{|title|:|" $2 "|,|param|:|" $4 ":" $6 "|}" }}'|sed -e 's/|/"/g'|jq -s > services_ips.json
-    scp services_ips.json s3:production/workspace.production/www/
-    run ./get_apis.sh
-
-    sudo snap alias microk8s.kubectl kubectl 
-    run microk8s.kubectl cluster-info
-    run microk8s.kubectl get nodes
-    run microk8s.kubectl get all -A
+    echo '  output log of each reinit'
+    for h in {5..8}; do
+        cat /tmp/ssh.s$h.txt
+        rm /tmp/ssh.s$h.txt
+    done
+    rm "$SCRIPT"
+    logElapsed "${FUNCNAME[0]}"
 }
+
 # -----------------------------------------------------------------------------------
 function run() {
   {
