e0586ca312a15bc0729bdf6a6ab482552a21837a
Ballantyne, Bob <ballantyne.robert@gmail.com>
2022-06-19 14:25:49 +0000

    change patch files to yaml and restructure app i/face

--- capture_ips_for_gui.sh
+++ capture_ips_for_gui.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
 # declarations of MUST HAVE globals
-PROGRAM_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
-BASHLIB_DIR='/home/bobb/.bin/utilities/bashlib'
+PROGRAM_DIR=~/GIT/k8s
+PROGRAM_NAME="$(basename "${BASH_SOURCE[0]}" | sed 's|.sh$||')"
+LOGFILE="$(pwd)/${PROGRAM_NAME}.log"
+(
+  {
+    source "${PROGRAM_DIR}/k8s.bashlib"
+    k8s.__init
+    cd /home/bobb/workspace
 
-source "${PROGRAM_DIR}/k8s.bashlib"
-k8s.__init
+#    k8s.separator k8s.reinit_nodes
+#    k8s.separator k8s.add_nodes_to_cluster
+#    k8s.separator k8s.set_config
+#    k8s.separator k8s.enable_microk8s_modules
+#    k8s.separator k8s.customize_content
+#    k8s.separator k8s.add_new_content
+#    k8s.separator k8s.generate_token
+#    k8s.separator k8s.capture_config
+#    k8s.separator k8s.update_jenkins
+    k8s.separator k8s.capture_ips_for_gui
+#    k8s.separator k8s.capture_status
 
-cd /home/bobb/workspace
+  } 2>&1
+) | tee "${LOGFILE}"
 
-k8s.capture_ips_for_gui
+sed -i -E -e 's/\x1b\[[0-9;]*m//g' "${LOGFILE}"
+exit 0 
--- get_apis.sh
+++ get_apis.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
 # declarations of MUST HAVE globals
-PROGRAM_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
-BASHLIB_DIR='/home/bobb/.bin/utilities/bashlib'
+PROGRAM_DIR=~/GIT/k8s
+PROGRAM_NAME="$(basename "${BASH_SOURCE[0]}" | sed 's|.sh$||')"
+LOGFILE="$(pwd)/${PROGRAM_NAME}.log"
+{
+  declare -i status
+  (
+    source "${PROGRAM_DIR}/k8s.bashlib"
+    k8s.__init
+    cd /home/bobb/workspace
 
-source "${PROGRAM_DIR}/k8s.bashlib"
-k8s.__init
+    k8s.dump_apis
 
-cd /home/bobb/workspace
+  ) && status=$? || status=$? 2>&1
+  [ "$status" -eq 0 ] || echo "exit code ${status}"
 
-k8s.dump_apis
+} | tee "${LOGFILE}"
+
+sed -i -E -e 's/\x1b\[[0-9;]*m//g' "${LOGFILE}"
+exit 0
