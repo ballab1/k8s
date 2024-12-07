a048f41fcb7ef75f14a97f678a2946371f0e1399
Ballantyne, Bob <ballantyne.robert@gmail.com>
2024-12-07 11:20:12 -0500

    add v1.31 support

--- capture_ips_for_gui.sh
+++ capture_ips_for_gui.sh
@@ -3,13 +3,14 @@
 # declarations of MUST HAVE globals
 PROGRAM_DIR=~/GIT/k8s
 PROGRAM_NAME="$(basename "${BASH_SOURCE[0]}" | sed 's|.sh$||')"
-LOGFILE="$(pwd)/${PROGRAM_NAME}.log"
+WORKSPACE='/home/bobb/workspace'
+LOGFILE="${WORKSPACE}/current/${PROGRAM_NAME}.log"
+cd "${WORKSPACE}" ||:
+mkdir -p current ||:
+source "${PROGRAM_DIR}/k8s.bashlib"
 (
   {
-    source "${PROGRAM_DIR}/k8s.bashlib"
     k8s.__init
-    cd /home/bobb/workspace
-
 #    k8s.separator k8s.reinit_nodes
 #    k8s.separator k8s.add_nodes_to_cluster
 #    k8s.separator k8s.set_config
@@ -20,11 +21,10 @@ LOGFILE="$(pwd)/${PROGRAM_NAME}.log"
 #    k8s.separator k8s.capture_config
 #    k8s.separator k8s.update_jenkins
     k8s.separator k8s.capture_ips_for_gui
-    jq -r '[.[]|"  \(.title): \(.host):\(.port)"]|sort[]' current/services_ips.json
 #    k8s.separator k8s.capture_status
 
   } 2>&1
 ) | tee "${LOGFILE}"
 
-sed -i -E -e 's/\x1b\[[0-9;]*m//g' "${LOGFILE}"
+sed -i -E -e "${k8s_NO_ASCII}" "${LOGFILE}"
 exit 0
--- get_apis.sh
+++ get_apis.sh
@@ -3,20 +3,21 @@
 # declarations of MUST HAVE globals
 PROGRAM_DIR=~/GIT/k8s
 PROGRAM_NAME="$(basename "${BASH_SOURCE[0]}" | sed 's|.sh$||')"
-LOGFILE="$(pwd)/${PROGRAM_NAME}.log"
+WORKSPACE='/home/bobb/workspace'
+LOGFILE="${WORKSPACE}/current/${PROGRAM_NAME}.log"
+cd "${WORKSPACE}" ||:
+mkdir -p current ||:
+source "${PROGRAM_DIR}/k8s.bashlib"
 {
   declare -i status
   (
-    source "${PROGRAM_DIR}/k8s.bashlib"
     k8s.__init
-    cd /home/bobb/workspace
-
-    k8s.dump_apis
+    k8s.capture_status
 
   ) && status=$? || status=$? 2>&1
   [ "$status" -eq 0 ] || echo "exit code ${status}"
 
 } | tee "${LOGFILE}"
 
-sed -i -E -e 's/\x1b\[[0-9;]*m//g' "${LOGFILE}"
+sed -i -E -e "${k8s_NO_ASCII}" "${LOGFILE}"
 exit 0
