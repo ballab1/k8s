bd5d47cad504f3a8a30d0afc986cff1294bf2252
Ballantyne, Bob <ballantyne.robert@gmail.com>
2024-12-31 17:58:01 -0500

    add devPi

--- restore_postgres.sh
+++ restore_postgres.sh
@@ -5,13 +5,15 @@ PROGRAM_DIR=~/GIT/k8s
 PROGRAM_NAME="$(basename "${BASH_SOURCE[0]}" | sed 's|.sh$||')"
 WORKSPACE='/home/bobb/workspace'
 LOGFILE="${WORKSPACE}/current/${PROGRAM_NAME}.log"
+SQL_FILE='2024-12-29.postgres.sql'
+
 cd "${WORKSPACE}" ||:
 mkdir -p current ||:
 source "${PROGRAM_DIR}/k8s.bashlib"
 (
   {
     k8s.__init
-    k8s.restore_postgres "$@"
+    k8s.restore_hostpath "$@"
 
   } 2>&1
 ) | tee "${LOGFILE}"
