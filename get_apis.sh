e54b11e0002a5e5b4600e6df8767975f7b450fd6
Ballantyne, Bob <ballantyne.robert@gmail.com>
2025-03-05 14:31:28 -0500

    update ingress defns and update scripts to be more consistent

--- get_apis.sh
+++ get_apis.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # declarations of MUST HAVE globals
-PROGRAM_DIR=~/GIT/k8s
+PROGRAM_DIR="$(readlink -f ~/GIT/k8s)"
 PROGRAM_NAME="$(basename "${BASH_SOURCE[0]}" | sed 's|.sh$||')"
 WORKSPACE='/home/bobb/workspace'
 LOGFILE="${WORKSPACE}/current/${PROGRAM_NAME}.log"
+
 cd "${WORKSPACE}" ||:
 mkdir -p current ||:
 source "${PROGRAM_DIR}/k8s.bashlib"
@@ -17,7 +18,7 @@ source "${PROGRAM_DIR}/k8s.bashlib"
   ) && status=$? || status=$? 2>&1
   [ "$status" -eq 0 ] || echo "exit code ${status}"
 
-} | tee "${LOGFILE}"
+} 2>&1 | tee "${LOGFILE}"
 
 sed -i -E -e "${k8s_NO_ASCII}" "${LOGFILE}"
 exit 0
