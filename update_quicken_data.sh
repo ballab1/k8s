519f30d455dbde438903fb7bcd181370c2bbc018
Ballantyne, Bob <ballantyne.robert@gmail.com>
2026-04-08 11:52:33 -0400

    updates for registry.ubuntu.home

---
+++ update_quicken_data.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+FILE=~/20251231-quicken.csv
+
+KUBECTL='/snap/bin/kubectl'
+POD="$("$KUBECTL" get pods -n postgres -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}')"
+"$KUBECTL" cp "$FILE" "postgres/${POD}:/tmp/transactions.csv"
+"$KUBECTL" exec -n postgres "${POD}" -- chmod 666 /tmp/transactions.csv
+"$KUBECTL" exec -n postgres "${POD}" -- psql -U bobb -h localhost -d money -c "truncate quicken_data"
+"$KUBECTL" exec -n postgres "${POD}" -- psql -U bobb -h localhost -d money -c "copy quicken_data from '/tmp/transactions.csv' header csv"
+"$KUBECTL" exec -n postgres "${POD}" -- rm /tmp/transactions.csv
+
+
